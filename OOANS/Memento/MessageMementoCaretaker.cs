da499fa0c674f87406ac28725f95be408735e68f
Jozef Schneider <jojdzino999@gmail.com>
2019-04-20 17:31:31 +0200

    Added memento

---
+++ OOANS/Memento/MessageMementoCaretaker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOANS.Memento
+{
+    public class MessageMementoCaretaker
+    {
+        public int StackSize = 10;
+        private IList<MessageDTO> _stack;
+
+        public MessageDTO GetMemento(int index)
+        {
+            return _stack[index];
+        }
+        
+        public bool CreateMemento(MessageDTO memento)
+        {
+            if(StackSize<=_stack.Count+1)
+            {
+                _stack.Add(memento);
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- OOANS/Message.cs
+++ OOANS/Message.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOANS.Memento;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,21 @@ namespace OOANS
         public string Text { get; set; }
         public User SentFrom { get; internal set; }
         public MessageDTO MessageDTO {get;set;}
-
+        private MessageMementoCaretaker _caretaker = new MessageMementoCaretaker();
         public override string ToString()
         {
             return Text;
+        }
+
+        public bool SaveMemento(MessageDTO memento)
+        {
+            return _caretaker.AddMemento(memento);
+        }
+
+        public MessageDTO RestoreMemento(int index)
+        {
+            this.MessageDTO = _caretaker.GetMemento(index);
+            return this.MessageDTO;
         }
     }
 }
