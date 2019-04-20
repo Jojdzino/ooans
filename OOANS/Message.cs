4880ace93efef00116a01ea007bf625c611b1837
Jozef Schneider <jojdzino999@gmail.com>
2019-04-20 17:23:48 +0200

    Added DTO

--- OOANS/Message.cs
+++ OOANS/Message.cs
@@ -10,6 +10,7 @@ namespace OOANS
     {
         public string Text { get; set; }
         public User SentFrom { get; internal set; }
+        public MessageDTO MessageDTO {get;set;}
 
         public override string ToString()
         {
